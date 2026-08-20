c2a8d902729f91db8e7d7683ca838ca01fd2a124
raghav-dev7 <raghavft07@gmail.com>
2026-08-20 16:03:35 +0530

    Add files via upload

---
+++ Reapt.java
@@ -0,0 +1,19 @@
+public class Reapt {
+    public static void main(String[]args)
+    {
+        String a = "bheem";
+        for (int i=0;i<a.length()-1;i++)
+            {
+                for (int j=i+1;j<a.length()-1;j++)
+                {
+                    if (a.charAt(i) == a.charAt(j))
+                    {
+                        System.out.println(" "+a.charAt(i));
+                        return;
+                    }
+                }
+
+        }
+    }
+    
+}
